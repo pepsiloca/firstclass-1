87739acac9876541b3c3f9eeb164f3f1c1ee6a53
mackliu <macklun@gmail.com>
2020-03-20 15:56:13 +0800

    命名方式

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+//宣告一個變數
 $a=1;
 echo $a;
 echo "<br>";
@@ -9,6 +10,13 @@ echo "<br>";
 print($a);
 echo "<br>";
 
+$studentInSchool="YES";
+
+
+/*
+宣告一個常數
+常數不可被變更
+*/
 define("PI",3.1415927);
 echo PI;
 echo PI*2*10;
