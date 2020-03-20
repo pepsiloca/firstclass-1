c74f2534fa7393b726b978c9f31f057375829336
mackliu <macklun@gmail.com>
2020-03-20 15:29:49 +0800

    變數與常數

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 
-echo 'hello world';
-$aa;
-$bb;
-$cc;
-echo "aaaa";
+$a=1;
+echo $a;
+echo "<br>";
+$a=2.1;
+echo $a;
+echo "<br>";
+print($a);
+echo "<br>";
+
+define("PI",3.1415927);
+echo PI;
+echo PI*2*10;
+
 ?>
