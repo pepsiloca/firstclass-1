7b7e90bb723946eaf9eaa92023755e9568131cd9
mackliu <macklun@gmail.com>
2020-03-25 09:04:12 +0800

    運算子結束

--- index.php
+++ index.php
@@ -19,4 +19,43 @@ echo "<hr>";
 $a=2;
 $b=5;
 echo $a+$b;
+echo "<hr>";
+//字串運算子
+
+$a="A";
+$b="B";
+echo $a;
+echo "<br>";
+echo $b;
+$c=$a . $b ;
+echo "<hr>";
+echo $c;
+echo "<hr>";
+//字串運算子2
+
+$a="A";
+$b=1;
+echo $a;
+echo "<br>";
+echo $b;
+$c=$a . $b ;
+$d=$a + $b ;
+echo "<hr>";
+echo $c;
+echo "<hr>";
+echo $d;
+echo "<hr>";
+//三元運算子
+$a=5;
+
+echo ($a>0)?"很貴":"免費";
+
+echo "<hr>";
+
+if($a>0){
+    echo "很貴";
+}else{
+    echo "免費";
+}
+
 ?>
