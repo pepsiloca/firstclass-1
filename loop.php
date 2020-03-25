8dffb7bbc8b4a257ff7f505c885267bc5d030c32
mackliu <macklun@gmail.com>
2020-03-25 15:53:57 +0800

    for迴圈-簡單九九乘法表

---
+++ loop.php
@@ -0,0 +1,128 @@
+<?php
+
+//for loop
+
+//for($i=0;$i<10;$i++)  => ++$i
+
+echo "<table border='1'>";
+for($i=1;$i<=9;$i=$i+1){
+
+    echo "<tr>";
+    for($j=1;$j<=9;$j++){
+
+        echo "<td>" . $j . " x " . $i . " = " . ($i * $j) ."</td>";
+        
+    }
+    echo "</tr>";
+
+}
+
+echo "</table>";
+
+
+?>
+
+table *1
+tr *9
+td*81 => 9 *9 
+<table border=1>
+    <tr>
+        <td>1 x 1 = 10</td>
+        <td>2 x 1 = 10</td>
+        <td>3 x 1 = 10</td>
+        <td>4 x 1 = 10</td>
+        <td>5 x 1 = 10</td>
+        <td>6 x 1 = 10</td>
+        <td>7 x 1 = 10</td>
+        <td>8 x 1 = 10</td>
+        <td>9 x 1 = 10</td>
+    </tr>
+    <tr>
+    <td>1 x 1 = 10</td>
+        <td>2 x 1 = 10</td>
+        <td>3 x 1 = 10</td>
+        <td>4 x 1 = 10</td>
+        <td>5 x 1 = 10</td>
+        <td>6 x 1 = 10</td>
+        <td>7 x 1 = 10</td>
+        <td>8 x 1 = 10</td>
+        <td>9 x 1 = 10</td>
+    </tr>
+    <tr>
+    <td>1 x 1 = 10</td>
+        <td>2 x 1 = 10</td>
+        <td>3 x 1 = 10</td>
+        <td>4 x 1 = 10</td>
+        <td>5 x 1 = 10</td>
+        <td>6 x 1 = 10</td>
+        <td>7 x 1 = 10</td>
+        <td>8 x 1 = 10</td>
+        <td>9 x 1 = 10</td>
+    </tr>
+    <tr>
+    <td>1 x 1 = 10</td>
+        <td>2 x 1 = 10</td>
+        <td>3 x 1 = 10</td>
+        <td>4 x 1 = 10</td>
+        <td>5 x 1 = 10</td>
+        <td>6 x 1 = 10</td>
+        <td>7 x 1 = 10</td>
+        <td>8 x 1 = 10</td>
+        <td>9 x 1 = 10</td>
+    </tr>
+    <tr>
+    <td>1 x 1 = 10</td>
+        <td>2 x 1 = 10</td>
+        <td>3 x 1 = 10</td>
+        <td>4 x 1 = 10</td>
+        <td>5 x 1 = 10</td>
+        <td>6 x 1 = 10</td>
+        <td>7 x 1 = 10</td>
+        <td>8 x 1 = 10</td>
+        <td>9 x 1 = 10</td>
+    </tr>
+    <tr>
+    <td>1 x 1 = 10</td>
+        <td>2 x 1 = 10</td>
+        <td>3 x 1 = 10</td>
+        <td>4 x 1 = 10</td>
+        <td>5 x 1 = 10</td>
+        <td>6 x 1 = 10</td>
+        <td>7 x 1 = 10</td>
+        <td>8 x 1 = 10</td>
+        <td>9 x 1 = 10</td>
+    </tr>
+    <tr>
+    <td>1 x 1 = 10</td>
+        <td>2 x 1 = 10</td>
+        <td>3 x 1 = 10</td>
+        <td>4 x 1 = 10</td>
+        <td>5 x 1 = 10</td>
+        <td>6 x 1 = 10</td>
+        <td>7 x 1 = 10</td>
+        <td>8 x 1 = 10</td>
+        <td>9 x 1 = 10</td>
+    </tr>
+    <tr>
+    <td>1 x 1 = 10</td>
+        <td>2 x 1 = 10</td>
+        <td>3 x 1 = 10</td>
+        <td>4 x 1 = 10</td>
+        <td>5 x 1 = 10</td>
+        <td>6 x 1 = 10</td>
+        <td>7 x 1 = 10</td>
+        <td>8 x 1 = 10</td>
+        <td>9 x 1 = 10</td>
+    </tr>
+    <tr>
+    <td>1 x 1 = 10</td>
+        <td>2 x 1 = 10</td>
+        <td>3 x 1 = 10</td>
+        <td>4 x 1 = 10</td>
+        <td>5 x 1 = 10</td>
+        <td>6 x 1 = 10</td>
+        <td>7 x 1 = 10</td>
+        <td>8 x 1 = 10</td>
+        <td>9 x 1 = 10</td>
+    </tr>
+</table>
