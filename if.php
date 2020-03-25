b09fb5b1166e15711edf00e0ef1bc6b69f066bbd
mackliu <macklun@gmail.com>
2020-03-25 14:34:05 +0800

    三元運算子

--- if.php
+++ if.php
@@ -1,18 +1,30 @@
 <?php
 
 
-$score=50;
+$score=75;
 //if 判斷式
-
-
 
 if($score>=60){
     echo "及格";
 }else{
     echo "不及格";
 }
+$result="";
+if($score>=60){
+    $result= "及格";
+}else{
+    $result= "不及格";
+}
+echo $result;
 
 echo "<hr>";
+// 三元運算子
+echo "三元運算子<br>";
+
+$result=($score>=60)?"及格":"不及格";
+echo $result;
+echo "<hr>";
+
 //判斷成績
 /*
 100~90 -> “A”
@@ -78,5 +90,43 @@ if( $year%4 == 0  &&  ($year%100 !=0 || $year%400==0)){
     echo "平年";
 
 }
+echo "<hr>";
+
+
+$score=88;
+$level="";
+
+if($score>=90){
+    $level="A";
+}else if($score >=75){
+    $level="B";
+}else if($score >=60){
+    $level="C";
+}else {
+    $level="D";
+}
+
+echo "成績:" . $score;
+echo "<br>";
+echo "等級:".$level;
+echo "<br>";
+switch($level){
+    case "A":
+        echo "非常好，請努力保持";
+    break;
+    case "B":
+        echo "可圈可點，但還有進步的空間";
+    break;
+    case "C":
+        echo "一般水平，需要更多的努力";
+    break;
+    case "D":
+        echo "用心不足，請加強";
+    break;
+    default:
+    
+}
+
+
 
 ?>
